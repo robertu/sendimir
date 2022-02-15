cb033ccee1fc84ab859b03773b7f17462ef2b3ea
xxQbixx <99727380+xxQbixx@users.noreply.github.com>
2022-02-15 16:25:26 +0100

    Update ic.sh

--- ic.sh
+++ ic.sh
@@ -21,4 +21,5 @@ apt update
     lcdlv="'"maildir_home"'"
 #ustaw konfiguracje
 echo "dc_eximconfig_configtype=$exicif\dc_other_hostnames=$hstnm.$dnsnm\dc_local_interfaces=127.0.0.1 ; ::1\dc_readhost=$clint\dc_relay_domains=$lchst\dc_minimaldns=$mndns\dc_relay_nets=$rlnts\dc_smarthost=''\CFILEMODE='644'\dc_use_split_config='false'\dc_hide_mailname=''\dc_mailname_in_oh='true'\dc_localdelivery='maildir_home$lcdlv" | /etc/exim4/update-exim4.conf.conf
-#zainstaluj exim4 na konfiguracjii
+#zainstaluj exim4 na konfiguracji
+apt install exim4
