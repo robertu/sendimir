fc27ad3d11f9dea403080b53e6b6baf160e8ba09
xxQbixx <99727380+xxQbixx@users.noreply.github.com>
2022-02-16 10:26:20 +0100

    Update ic.sh

--- ic.sh
+++ ic.sh
@@ -19,19 +19,19 @@ c="'"
     rlnts="'"cgi.com"'"
     #dc_localdelivery='maildir_home
     lcdlv="'"maildir_home"'"
-#ustaw konfiguracje
-echo "dc_eximconfig_configtype=$exicf" > /etc/exim4/update-exim4.conf.conf
-echo "dc_other_hostnames=$hstnm.$dnsnm" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_local_interfaces=127.0.0.1 ;::1" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_readhost=$clint" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_relay_domains=$lchst" > /etc/exim4/update-exim4.conf.conf
-echo "adc_minimaldns=$mndns" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_relay_nets=$rlnts" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_smarthost=''" > /etc/exim4/update-exim4.conf.conf
-echo "x0aCFILEMODE='644'" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_use_split_config='false'" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_hide_mailname=''" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_mailname_in_oh='true'" > /etc/exim4/update-exim4.conf.conf
-echo "x0adc_localdelivery='maildir_home$lcdlv" > /etc/exim4/update-exim4.conf.conf
+#ustaw konfiguracje.
+rm -r /etc/exim4/update-exim4.conf.conf
+echo "dc_eximconfig_configtype=$exicf" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_other_hostnames=$hstnm.$dnsnm" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_local_interfaces='127.0.0.1 ;::1'" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_readhost=$clint" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_relay_domains=$lchst" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_minimaldns=$mndns" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_relay_nets=$rlnts" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_smarthost=''" >> /etc/exim4/update-exim4.conf.conf
+echo "CFILEMODE='644'" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_use_split_config='false'" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_mailname_in_oh='true'" >> /etc/exim4/update-exim4.conf.conf
+echo "dc_localdelivery=$lcdlv" >> /etc/exim4/update-exim4.conf.conf
 #zainstaluj exim4 na konfiguracjii
 #apt install exim4
