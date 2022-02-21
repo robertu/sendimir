b98b39e3416c449a4e4d4314eac648a057419e51
xxQbixx <99727380+xxQbixx@users.noreply.github.com>
2022-02-21 14:50:39 +0100

    Update iredmail.sh

--- IRedMail/iredmail.sh
+++ IRedMail/iredmail.sh
@@ -3,7 +3,7 @@ wget https://github.com/iredmail/iRedMail/archive/refs/tags/1.5.1.tar.gz
 #wypakuj
 tar -xvf 1.5.1.tar.gz
 #pobierz konfiguracje
-cat config2 >> iRedmail-1.5.1/config
+cat /tmp/config2 >> /root/iRedmail-1.5.1/config
 #zainstaluj
 cd iRedMail-1.5.1/
 
