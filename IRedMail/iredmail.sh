2676474932327f037ee17b55253d7619602acc80
xxQbixx <99727380+xxQbixx@users.noreply.github.com>
2022-02-21 15:00:29 +0100

    Update iredmail.sh

--- IRedMail/iredmail.sh
+++ IRedMail/iredmail.sh
@@ -1,17 +1,20 @@
+#!/bin/bash
 #pobierz paczke
 wget https://github.com/iredmail/iRedMail/archive/refs/tags/1.5.1.tar.gz
 #wypakuj
 tar -xvf 1.5.1.tar.gz
+#przenieś
+mv iRedMail-1.5.1 /root/
 #pobierz konfiguracje
 cat /tmp/config2 >> /root/iRedmail-1.5.1/config
 #zainstaluj
 cd iRedMail-1.5.1/
-
-    AUTO_USE_EXISTING_CONFIG_FILE=y \
-    AUTO_INSTALL_WITHOUT_CONFIRM=y \
-    AUTO_CLEANUP_REMOVE_SENDMAIL=y \
-    AUTO_CLEANUP_REPLACE_FIREWALL_RULES=y \
-    AUTO_CLEANUP_RESTART_FIREWALL=y \
-    AUTO_CLEANUP_REPLACE_MYSQL_CONFIG=y \
+    #ustaw variables do cichej instalacji
+export AUTO_USE_EXISTING_CONFIG_FILE=y \
+export AUTO_INSTALL_WITHOUT_CONFIRM=y \
+export AUTO_CLEANUP_REMOVE_SENDMAIL=y \
+export AUTO_CLEANUP_REPLACE_FIREWALL_RULES=y \
+export AUTO_CLEANUP_RESTART_FIREWALL=y \
+export AUTO_CLEANUP_REPLACE_MYSQL_CONFIG=y \
     
-bash iRedMail.sh
+bash iRedMail.sh #odpal skrypt instalacyjny
