a5fca8823ffad612444e3a8e5ee2ba8a268e4d9b
xxQbixx <99727380+xxQbixx@users.noreply.github.com>
2022-02-23 10:16:56 +0100

    Add files via upload

--- IRedMail/iredmail.sh
+++ IRedMail/iredmail.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#pobierz paczke
-wget https://github.com/iredmail/iRedMail/archive/refs/tags/1.5.1.tar.gz
-#wypakuj
-tar -xvf 1.5.1.tar.gz
-#przenieś
-mv iRedMail-1.5.1 /home/$USER
-#pobierz konfiguracje
-cat /tmp/config2 >> /home/$USER/iRedmail-1.5.1/config
-#zainstaluj
-cd /home/$USER/iRedMail-1.5.1/
     #ustaw variables do cichej instalacji
 export AUTO_USE_EXISTING_CONFIG_FILE=y \
 export AUTO_INSTALL_WITHOUT_CONFIRM=y \
@@ -17,4 +7,4 @@ export AUTO_CLEANUP_REPLACE_FIREWALL_RULES=y \
 export AUTO_CLEANUP_RESTART_FIREWALL=y \
 export AUTO_CLEANUP_REPLACE_MYSQL_CONFIG=y \
     
-bash iRedMail.sh #odpal skrypt instalacyjny
+./iRedMail.sh #odpal skrypt instalacyjny
